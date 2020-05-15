a82d6e37d575eafb4d6bca79547905a6ad0a5536
cockscomb <cockscomb@hatena.ne.jp>
2020-05-15 11:57:17 +0900

    Ask name

--- main.go
+++ main.go
@@ -5,19 +5,44 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 )
 
-func main() {
-	confirm := false
+func askDoYouWantToApply() (bool, error) {
+	confirmation := false
 	prompt := &survey.Confirm{
 		Message: "Do you want to apply for our internship?",
 	}
-	err := survey.AskOne(prompt, &confirm)
+	if err := survey.AskOne(prompt, &confirmation); err != nil {
+		return confirmation, err
+	}
+	return confirmation, nil
+}
+
+func askName() (string, error) {
+	name := ""
+	prompt := &survey.Input{
+		Message:  "What is your name?",
+	}
+	if err := survey.AskOne(prompt, &name); err != nil {
+		return name, err
+	}
+	return name, nil
+}
+
+func main() {
+	wantToApply, err := askDoYouWantToApply()
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	if !confirm {
+	if !wantToApply {
 		fmt.Println("See you again.")
 		return
 	}
 	fmt.Println("Thank you!")
+
+	name, err := askName()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Printf("Hello, %s.", name)
 }
